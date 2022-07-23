91d841bf3c5f604c771c899d6cb08ff2079736e5
yuri2 <yuri2peter@qq.com>
2022-07-23 23:23:25 +0800

    环境变量

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/* eslint-disable node/no-unpublished-import */
+import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  envPrefix: 'FRONTEND_', // 以 envPrefix 开头的环境变量会通过 import.meta.env 暴露在你的客户端源码中
+  resolve: {
+    alias: {
+      'src/': `${path.resolve(__dirname, 'src')}/`, // 别名，指向 src 目录
+    },
+  },
+  server: {
+    port: 3000, // 开发服务器端口
+    host: true, // 监听所有地址，包括局域网和公网地址
+  },
+  preview: {
+    port: 80, // 预览服务器端口
+    host: true, // 监听所有地址，包括局域网和公网地址
+    strictPort: true, // 端口被占用时，抛出错误
+  },
+});
