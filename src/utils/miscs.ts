4c892b88386e8aa15988d78fc99cf38a980b0dfd
yuri2 <yuri2peter@qq.com>
2022-07-24 02:43:01 +0800

    utils

--- src/utils/miscs.ts
+++ src/utils/miscs.ts
@@ -81,3 +81,22 @@ export function getCurrentTimeSeconds() {
 export function getZeroIfLessThanZero(num: number) {
   return num < 0 ? 0 : num;
 }
+
+// 等待
+export function sleep(time: number): Promise<void> {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, time);
+  });
+}
+
+// 保留3位小数
+export function fixNumber(num: number) {
+  return Math.round(num * 1000) / 1000;
+}
+
+// 限制数字在某一范围内
+export function clamp(num: number, min: number, max: number) {
+  return Math.min(Math.max(num, min), max);
+}
