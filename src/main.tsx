4785c64afa86fdc69240f55e2bee6adc06212fad
yuri2 <yuri2peter@qq.com>
2022-07-24 02:55:40 +0800

    DataPicker

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import zhLocale from 'date-fns/locale/zh-CN';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <LocalizationProvider dateAdapter={AdapterDateFns} locale={zhLocale}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </LocalizationProvider>
   </React.StrictMode>
 );
